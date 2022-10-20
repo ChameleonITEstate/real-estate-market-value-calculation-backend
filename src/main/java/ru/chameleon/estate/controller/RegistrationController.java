dd389ccbb694f2d632ff1e63e4bee5eb21df4e64
Kenseax <70656830+Kenseax@users.noreply.github.com>
2022-10-20 19:06:19 +0300

    preparing to heroku deploy

--- src/main/java/ru/chameleon/estate/controller/RegistrationController.java
+++ src/main/java/ru/chameleon/estate/controller/RegistrationController.java
@@ -1,13 +1,10 @@
 package ru.chameleon.estate.controller;
 
-import org.springframework.http.ResponseEntity;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.context.request.WebRequest;
 
 @RestController
 @RequestMapping("/registration/api")
