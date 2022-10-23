26c65257452e3918c09f89b6b17140f0da81e2de
Kenseax <70656830+Kenseax@users.noreply.github.com>
2022-10-23 18:50:26 +0300

    CORS filter

--- src/main/java/ru/chameleon/estate/security/config/SimpleCORSFilter.java
+++ src/main/java/ru/chameleon/estate/security/config/SimpleCORSFilter.java
@@ -18,7 +18,7 @@ public class SimpleCORSFilter implements Filter {
         response.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
         response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
+        response.setHeader("Access-Control-Allow-Headers", "Content-type");
         chain.doFilter(req, res);
     }
 
