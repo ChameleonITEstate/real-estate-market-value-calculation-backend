54a58ad8e3d3d34f2deee926030afeb6b08c2a2f
Kenseax <70656830+Kenseax@users.noreply.github.com>
2022-10-24 19:37:52 +0300

    Corrected EstateEntity

--- src/main/java/ru/chameleon/estate/entity/EstateEntity.java
+++ src/main/java/ru/chameleon/estate/entity/EstateEntity.java
@@ -22,20 +22,74 @@ public class EstateEntity {
     @Column(name = "estate_id")
     private Long estateId;
     /**
+     * necessary
      * Full Address
      */
     @OneToOne(mappedBy = "estate", cascade = CascadeType.ALL)
     private FullAddress fullAddress;
     /**
+     * necessary
+     * Number of Rooms
+     */
+    @Column(name = "number_of_rooms")
+    private Integer numberOfRooms;
+    /**
+     * necessary
+     * Number of Rooms
+     */
+    @Column(name = "number_of_floors")
+    private Integer numberOfFloors;
+    /**
+     * necessary
+     * New building or Old building
+     */
+    @Column(name = "segment")
+    private String segment;
+    /**
+     * necessary
+     * Bricks, panels or monolith
+     */
+    @Column(name = "material_of_building")
+    private String materialOfBuilding;
+
+    /**
+     * could be corrected
+     * Number of current apartment floor
+     */
+    @Column(name = "current_floor")
+    private Integer currentFloor;
+    /**
+     * could be corrected
      * Metric Area
      */
     @Column(name = "metric_area")
     private Float metricArea;
     /**
-     * Number of Rooms
+     * could be corrected
+     * Metric of Kitchen
      */
-    @Column(name = "number_of_rooms")
-    private Integer numberOfRooms;
+    @Column(name = "metric_kitchen")
+    private Float metricKitchen;
+    /**
+     * could be corrected
+     * The presence of a balcony
+     */
+    @Column(name = "has_balcony")
+    private boolean hasBalcony;
+    /**
+     * could be corrected
+     * Distance to the nearest metro
+     */
+    @Column(name = "distance_to_metro")
+    private Float distanceToMetro;
+    /**
+     * could be corrected
+     * State of apartment:
+     * without renovation, renovated apartment, municipal renovation
+     */
+    @Column(name = "state")
+    private String state;
+
 
     @Override
     public boolean equals(Object o) {
